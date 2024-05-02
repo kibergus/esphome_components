f880243ba8a8ef3bbc8e33455f31b4dac944e3e5
Alexey Guseynov <kibergus@gmail.com>
2024-05-02 18:41:14 +0100

    Adapt the code to support SI115X sensor (found in Grove - Sunlight Sensor). Support for si1145 dropped.

--- components/si1145/si1145.h
+++ components/si1145/si1145.h
@@ -92,6 +92,25 @@ static const uint8_t SI1145_REG_CHIPSTAT = 0x30;
 static const uint16_t VALUE_AT_ZERO_HIGH = 260;
 static const uint16_t VALUE_AT_ZERO_LOW = 270;
 
+static const uint8_t SI115X_RESPONSE_0 = 0x11;
+static const uint8_t SI115X_RESPONSE_1 = 0x10;
+static const uint8_t SI115X_COMMAND = 0x0B;
+static const uint8_t SI115X_HOSTIN_0 = 0x0A;
+static const uint8_t HOSTOUT_0 = 0x13;
+static const uint8_t HOSTOUT_1 = 0x14;
+static const uint8_t HOSTOUT_2 = 0x15;
+static const uint8_t HOSTOUT_3 = 0x16;
+static const uint8_t SI115X_ALS_FORCE = 0x11;
+static const uint8_t SI115X_ALS_START = 0x13;
+
+static const uint8_t CHAN_LIST = 0x01;
+static const uint8_t ADCCONFIG_0 = 0x02;
+static const uint8_t MEASCONFIG_0 = 0x05;
+static const uint8_t ADCPOST_0 = 0x04;
+static const uint8_t ADCCONFIG_1 = 0x06;
+static const uint8_t MEASCONFIG_1 = 0x09;
+static const uint8_t ADCPOST_1 = 0x08;
+
 #define SI1145_REG_UVINDEX0 0x2C
 #define SI1145_REG_UVINDEX1 0x2D
 #define SI1145_PARAM_PSADCGAIN 0x0B
@@ -104,23 +123,12 @@ static const uint16_t VALUE_AT_ZERO_LOW = 270;
 
 #define OVERFLOW_VALUE 0x7FFF
 
-enum Range { RANGE_HIGH = 0x20, RANGE_LOW = 0x00 };
-
 /// This class implements support for the SI1145 i2c sensor.
 class SI1145Component : public PollingComponent, public i2c::I2CDevice {
  public:
   void set_visible_sensor(sensor::Sensor *visible_sensor) { visible_sensor_ = visible_sensor; }
   void set_infrared_sensor(sensor::Sensor *infrared_sensor) { infrared_sensor_ = infrared_sensor; }
-  void set_uvindex_sensor(sensor::Sensor *uvindex_sensor) { uvindex_sensor_ = uvindex_sensor; }
   void set_illuminance_sensor(sensor::Sensor *illuminance_sensor) { illuminance_sensor_ = illuminance_sensor; }
-  void set_visible_auto(bool v) { visible_mode_auto_ = v; }
-  void set_infrared_auto(bool v) { infrared_mode_auto_ = v; }
-  void set_visible_temp_correction(bool v) { visible_temp_correction_ = v; }
-  void set_infrared_temp_correction(bool v) { infrared_temp_correction_ = v; }
-  void set_visible_range(Range v) { visible_range_ = v; }
-  void set_infrared_range(Range v) { infrared_range_ = v; }
-  void set_visible_gain(uint8_t v) { visible_gain_ = v; }
-  void set_infrared_gain(uint8_t v) { infrared_gain_ = v; }
 
   // ========== INTERNAL METHODS ==========
   // (In most use cases you won't need these)
@@ -130,60 +138,21 @@ class SI1145Component : public PollingComponent, public i2c::I2CDevice {
   void update() override;
 
  protected:
-  // Read visible light
-  uint16_t read_visible_();
-  // Read infrared light
-  uint16_t read_infrared_();
-  // Read UV factor
-  uint8_t read_uvindex_();
-  // Read temp
-  uint16_t read_temp_();
   // Begin
   bool begin_();
-  // Reset
-  void reset_();
+
   // Aux RW fns
   void write8_(uint8_t reg, uint8_t val);
   uint8_t read8_(uint8_t reg);
-  uint16_t read16_(uint8_t reg);
-  uint8_t write_param_(uint8_t p, uint8_t v);
 
-  void auto_range_visible_(uint16_t read_value);
-  void auto_range_infrared_(uint16_t read_value);
-
-  void set_visible_gain_(uint8_t gain);
-
-  void set_infrared_gain_(uint8_t gain);
-
-  void set_visible_range_(uint8_t range);
-
-  void set_infrared_range_(uint8_t range);
+  void param_set(uint8_t loc, uint8_t val);
+  void send_command(uint8_t code);
 
   // Sensors
   sensor::Sensor *visible_sensor_;
   sensor::Sensor *infrared_sensor_;
   sensor::Sensor *uvindex_sensor_;
   sensor::Sensor *illuminance_sensor_;
-
-  // Settings
-  Range visible_range_ = Range::RANGE_LOW;
-  Range infrared_range_ = Range::RANGE_LOW;
-  uint8_t visible_gain_ = 0;
-  uint8_t infrared_gain_ = 0;
-
-  bool visible_mode_auto_ = true;
-  bool infrared_mode_auto_ = true;
-
-  bool visible_temp_correction_ = false;
-  bool infrared_temp_correction_ = false;
-
-  uint16_t temp_at_begin_ = 0;
-
-  enum ErrorCode {
-    NONE = 0,
-    COMMUNICATION_FAILED,
-    WRONG_CHIP_ID,
-  } error_code_{NONE};
 };
 
 }  // namespace si1145
